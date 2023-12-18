abd359eba9977b18c8ee6809da8e11e4cb3d6e9e
Sanchir01 <emgushovs@mail.ru>
2023-12-18 16:01:17 +0300

    port3

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
 		origin: process.env.CLIENT_URL
 	})
 	app.use(cookieParser())
-	await app.listen(5000, process.env.PORT)
+	await app.listen(process.env.PORT)
 }
 bootstrap()
